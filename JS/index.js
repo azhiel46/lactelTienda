bcb5eaec73845091bfd609f78a04dd7b3fcf97dc
azhiel46 <aziel_baloacamp56@hotmail.com>
2022-11-02 20:21:18 -0600

    first commit

---
+++ JS/index.js
@@ -0,0 +1,43 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal('#introID', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '-100px'
+});
+
+sr.reveal('#aboutID', {
+  duration: 3000,
+  origin: 'bottom',
+  distance: '-100px'
+});
+
+sr.reveal('#productsID', {
+  duration: 3000,
+  origin: 'left',
+  distance: '-100px'
+});
+
+sr.reveal('#locationID', {
+  duration: 3000,
+  origin: 'right',
+  distance: '-100px'
+});
+
